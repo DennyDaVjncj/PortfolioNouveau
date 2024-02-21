7d59c4edce5c58150fd38e2fefc187e78ab2f738
DennyDaVjncj <daVjncjCode@gmail.com>
2024-02-21 13:13:23 -0500

    preliminary frontend code configuration

---
+++ lib/pages/hq.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:the_portfolio/pages/hq.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key})
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body:Center(child: Text('da Vjncj Code'),
+    )
+  }
+}
