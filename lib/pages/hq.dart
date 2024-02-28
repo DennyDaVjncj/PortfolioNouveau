68d7c6ca2c0d8a7feb0d4bcb29c4eb3d6b0834d9
DennyDaVjncj <daVjncjCode@gmail.com>
2024-02-27 22:11:11 -0500

    hq configuration update. debuged class construct

---
+++ lib/pages/hq.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Hq extends StatefulWidget {
+  const Hq({super.key});
+
+  @override
+  State<Hq> createState() => _Hq();
+}
+
+class _Hq extends State<Hq> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Text('le da Vjncj Code'),
+    ));
+  }
+}
